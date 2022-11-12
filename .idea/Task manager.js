e726f68150594ef0ae630b19041acdbff3cd064a
antoniu.pitic <antoniu.pitic@gmail.com>
2022-11-12 12:17:02 +0200

    Functie de adaugare de linie, apelata pe butonul albastru TODO - linia inca nu contine imaginile cu actiuni

--- .idea/Task manager.js
+++ .idea/Task manager.js
@@ -1,10 +1,28 @@
-function addRows(){
-    console.log("franzela!");
+function addRow(){
+    console.log("Adaug un rand in tabel");
+
+    const row = document.createElement('tr');
+
+    const idTd = document.createElement('td');
+    const titleTd = document.createElement('td');
+    const priorityTd = document.createElement('td');
+    const actionTd = document.createElement('td');
+
+    idTd.innerHTML = "55";
+    titleTd.innerHTML = "Linie adaugata din JS";
+    priorityTd.innerHTML = "8";
+    actionTd.innerHTML = "Aici o sa fie imaginile";
+
+    row.append(idTd, titleTd, priorityTd, actionTd);
+
+    const tasksTable = document.getElementById('tasksTable');
+
+    tasksTable.append(row);
 }
 
 function deleteTask(){
     console.log("Aici sterg un rand");
 }
 window.onload = (event) =>{
-    addRows();
+    //addRows();
 }
