3bee8b698dd1b0ec8d52969edc6a06f1522ccaf7
antoniu.pitic <antoniu.pitic@gmail.com>
2022-11-12 16:58:50 +0200

    u napel de update, inca nu e testat

--- .idea/Task editor.js
+++ .idea/Task editor.js
@@ -9,6 +9,32 @@ async function populateTextBoxes(id) {
     priorityTextbox.value = task.priority;
 }
 
+function updateOrAddTask(){
+    const queryString = window.location.search;
+    const params = new URLSearchParams(queryString);
+
+    const id = params.get('id');
+
+    if (id==0){
+        addTask();
+    }
+    else{
+        updateTask(id);
+    }
+}
+
+async function updateTask(id) {
+    const title = document.getElementById('title').value;
+    const priority = document.getElementById('priority').value;
+
+    const response = await fetch('http://localhost:8080/task/update?id=' + id + "&title" + title + "&priority" + priority, {method: 'PUT'});
+    goToTaskList();
+}
+
+function goToTaskList(){
+    window.location.href = "Task manager.html";
+}
+
 window.onload = (event) =>{
     const queryString = window.location.search;
     const params = new URLSearchParams(queryString);
